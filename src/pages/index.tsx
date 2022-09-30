4652cff2ff706f0c81312bca7a4db6eba45a1eed
David Glogaza <david.glogaza40@googlemail.com>
2022-09-30 20:33:23 +0200

    don't let loading block whole page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,20 @@ import { trpc } from "../utils/trpc";
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery(["questions.get-all-my-questions"]);
 
-  if (isLoading || !data) return <div>Loading...</div>;
-
   return (
     <>
       <div className="pt-6 gap-3 flex flex-col items-center w-full min-h-screen bg-gray-900">
         <QuestionCreator />
-        <p className="font-bold text-xl">Questions: </p>
-        {data.map((question) => (
-          <Link key={question.id} href={"/" + question.id}>
-            <div className="flex flex-row cursor-pointer">
-              <p className="font-bold text-lg">{question.question}</p>
-            </div>
-          </Link>
-        ))}
+        <p className="font-bold text-xl">Your Questions: </p>
+        {isLoading && <div>Loading...</div>}
+        {data &&
+          data.map((question) => (
+            <Link key={question.id} href={"/" + question.id}>
+              <div className="flex flex-row cursor-pointer">
+                <p className="font-bold text-lg">{question.question}</p>
+              </div>
+            </Link>
+          ))}
       </div>
     </>
   );
