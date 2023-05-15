8f58826992ff93f542f5cb7b97beb80a6384a5db
David Glogaza <david.glogaza40@googlemail.com>
2023-05-16 00:39:00 +0200

    change icon colors

--- src/components/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle.tsx
@@ -15,10 +15,6 @@ const ColorSchemeToggle = () => {
             theme.colorScheme === "dark"
               ? theme.colors.dark[6]
               : theme.colors.gray[0],
-          color:
-            theme.colorScheme === "dark"
-              ? theme.colors.yellow[4]
-              : theme.colors.blue[6],
         })}
       >
         {colorScheme === "dark" ? (
