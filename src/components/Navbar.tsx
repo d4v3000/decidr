ee7da09d4b0085c1a508c2b06d829320b97c7213
David Glogaza <david.glogaza40@googlemail.com>
2023-05-16 00:44:00 +0200

    fix github routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,8 @@
 import { ActionIcon, Header, Text } from "@mantine/core";
 import ColorSchemeToggle from "./ColorSchemeToggle";
 import { IconBrandGithubFilled } from "@tabler/icons-react";
-import { useRouter } from "next/router";
 
 const Navbar = () => {
-  const router = useRouter();
-
   return (
     <Header height={{ base: 50, md: 60 }}>
       <div className="mx-auto flex h-full w-full items-center justify-between px-4 md:w-3/4 md:px-0">
@@ -21,7 +18,8 @@ const Navbar = () => {
                   : theme.colors.gray[0],
             })}
             size="xl"
-            onClick={() => router.push("https://www.github.com/d4v3000/decidr")}
+            component="a"
+            href="https://www.github.com/d4v3000/decidr"
           >
             <IconBrandGithubFilled />
           </ActionIcon>
